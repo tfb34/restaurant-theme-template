6a13018a4ac300697effea88f96ab0439c06a785
Thalia <thalia.brito@rutgers.edu>
2018-11-06 15:22:19 -0500

    Complete mobile-responsive design for homepage

--- myScripts.js
+++ myScripts.js
@@ -1,6 +1,6 @@
 /*Carousel on index page*/
-var arr = ["one", "two", "three"];
-var dotArray = ["dot1", "dot2", "dot3"];
+var arr = ["one", "two", "three","four","five","six","seven","eight","nine"];
+var dotArray = ["dot1", "dot2", "dot3","dot4","dot5","dot6","dot7","dot8","dot9"];
 let currIndex = 0;
 /*move to the next picture in array of slideshow images*/
 function next(){
@@ -53,4 +53,26 @@ function previous(){
 		document.getElementById(arr[currIndex]).classList.remove("carousel-right");
 		currIndex = newCurrIndex;
 	},400);
+}
+
+/* Automatic slideshow*/
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
